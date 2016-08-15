7fbd158addacbdeb1758764e134585a9cbc5c611
ChoboHub <antoine_lucas_999@Hotmail.com>
2016-08-15 14:15:28 -0400

    Fix missing validator in database - Added update (needed to add validator to older version) - New release

--- extension.driver.php
+++ extension.driver.php
@@ -11,9 +11,21 @@
 				CREATE TABLE `tbl_fields_email` (
 					`id` int(11) unsigned NOT NULL auto_increment,
 					`field_id` int(11) unsigned NOT NULL,
+					`validator` varchar(255) default NULL,
 					PRIMARY KEY  (`id`),
 					KEY `field_id` (`field_id`)
 				) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 			");
+		}
+
+		public function update($previousVersion = false){
+
+			if (version_compare($previousVersion, '1.2.2', '<=')) {
+				Symphony::Database()->query(
+					"ALTER TABLE `tbl_fields_email`
+					ADD COLUMN `validator` varchar(255) default NULL;"
+				);
+			}
+
 		}
 	}
